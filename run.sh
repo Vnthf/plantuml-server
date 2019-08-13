7533e7ae21448307bc235088bda58018f2360e45
minhochoi <yovnthf1@naver.com>
2019-08-13 18:40:05 +0900

    docker-compose.yml 추가 및 스크립트 추가

---
+++ run.sh
@@ -0,0 +1,18 @@
+#. ./.env
+REGISTRY=127.0.0.1:5000
+REPLICA=2
+TAG=on-promise
+SERVICE=plantuml-server
+
+if [[ -z "${SERVER}" ]]; then
+  echo "Fail: export server config"
+  exit 1
+else
+  MY_SCRIPT_VARIABLE="${SERVER}"
+fi
+
+docker build -t 127.0.0.1:5000/plantuml-server:${TAG} .
+docker save 127.0.0.1:5000/plantuml-server:${TAG}
+ssh irteam@${SERVER} "docker load"
+ssh irteam@${SERVER} "docker push ${REGISTRY}/${SERVICE}:${TAG}"
+ssh irteam@${SERVER} "docker stack deploy -c docker-compose.yml plantuml"
