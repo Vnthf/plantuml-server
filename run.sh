184542177c490a4be71c447367b01514fb22c67e
minhochoi <yovnthf1@naver.com>
2019-09-05 14:13:35 +0900

    toast deploy 대응 shell 추가

--- run.sh
+++ run.sh
@@ -27,7 +27,22 @@ if [ $env == "local" ]; then
 	echo local
 	docker build -t 127.0.0.1:5000/${service}:${tag} .
 	docker-compose config | docker stack deploy -c - ${service}
-	exit 1
+	exit 0
+fi
+
+timestamp=$(read_var TIMESTAMP)
+useToastDeploy=$(read_var USE_TOAST_DEPLOY)
+echo "timestamp: ${timestamp}"
+
+# docker image 빌드
+echo "push docker build image 127.0.0.1:5000/${service}:${tag}"
+docker build --build-arg TIMESTAMP=${timestamp} -t 127.0.0.1:5000/${service}:${tag} .
+
+# toast deploy 를 이용
+if [ "$useToastDeploy" == true ] ; then
+	docker save -o ${service}.tar 127.0.0.1:5000/${service}:${tag}
+	echo "build success ${service}.tar"
+	exit 0
 fi
 
 # SERVER 값 필수
@@ -37,8 +52,17 @@ if [[ -z "${SERVER}" ]]; then
 fi
 
 echo "push docker image to ${server}"
-# docker image 빌드
-docker build -t 127.0.0.1:5000/${service}:${tag} .
-docker save 127.0.0.1:5000/${service}:${tag} | ssh irteam@${server} "docker load"
-ssh irteam@${server} "docker push ${registry}/${service}:${tag}"
-docker-compose config | ssh irteam@${server} "docker stack deploy -c - ${service}"
+
+# dev beta alpha의 경우는 ci의 registry 서버를 사용. 환경변수에 registry 설정해야 함
+if [ [$env == "dev" ] || [ $env == "beta" ] || [ $env == "alpha" ] ]; then
+	docker push 127.0.0.1:5000/${service}:${tag}
+	docker-compose config | ssh irteam@${server} "docker stack deploy -c - ${service}"
+
+else
+	docker save 127.0.0.1:5000/${service}:${tag} | ssh irteam@${server} "docker load"
+    ssh irteam@${server} "docker push ${registry}/${service}:${tag}"
+    docker-compose config | ssh irteam@${server} "docker stack deploy -c - ${service}"
+fi
+
+
+
